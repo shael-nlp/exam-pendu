231997e8aa314f225654bb526ef7b54402be037f
shael <shaelium@gmail.com>
2024-12-11 22:02:24 +0100

    Ajout des fonctions

--- generate_dicts.py
+++ generate_dicts.py
@@ -8,9 +8,29 @@ dico_9_lettres.txt
 dico_10_lettres.txt
 On enlève les accents, les espaces, les tirets et les mots en double.
 """
+from unidecode import unidecode
 
-def lire_filtrer_mots(chemin_lexique, longueur):
-    return []
+def lire_filtrer_mots(chemin_lexique:str, longueur:int) -> list:
+    """
+    Lit le fichier à partir de "chemin_lexique",
+    Extrait le premier mot pour chaque ligne en retirant les tirets et espaces.
+    Si le mot est de la bonne longueur, neutralise les accents,
+    le met en majuscule pour les besoins du jeu
+    et l'ajoute dans un set pour être exporté
+    """
+    with open(chemin_lexique, 'r', encoding='utf8') as f:
+        dico = set()
+        texte = f.readlines()
+        if not texte:
+            raise ValueError("Fichier vide")
+        else:
+            for ligne in texte:
+                mot = ligne.strip().split(" ")[0].replace(' ', '').replace('-', '')
+                if len(mot) == longueur:
+                    # mis en uppercase sinon ça ne fonctionne pas
+                    mot = unidecode(mot).upper()
+                    dico.add(mot)
+    return list(dico)
 
 
 def ecrire_liste_mots(liste_mots:list, longueur:int) -> None:
--- solver.py
+++ solver.py
@@ -1,19 +1,25 @@
-import math
-from copy import copy
+def generate_valid_words(possible_words:list[str], letters_in_secret:list[tuple[str, int]], letters_not_in_secret:list[str]) -> list[str]:
+    """
+    Génère une liste de mots qui sont encore valides en fonction des lettres déjà jouées.
+    """
+    return [word for word in possible_words if all(word[position] == l for l, position in letters_in_secret)
+            and not any(l in word for l in letters_not_in_secret)]
 
-
-def generate_valid_words(possible_words, letters_in_secret, letters_not_in_secret):
-    return []
-
-
-
-
-def generate_best_letters(possible_words:list, letters_not_played:list[str], letters_in_secret, letters_not_in_secret):
-    return "Ajoutez votre implémentation ici"
-
-
-
-
+def generate_best_letters(possible_words:list[str], letters_not_played:list[str], letters_in_secret:[list[str]], letters_not_in_secret:[list[str]]) -> str:
+    """
+    Calcule la meilleure lettre à jouer à partir de la
+    fréquence moyenne des lettres contenues dans les mots encore en jeu
+    """
+    letters = [l for l in letters_not_played if l not in letters_not_in_secret]
+    nombre_de_mots = len(possible_words)
+    best_letter = None
+    best_freq = -1
+    for l in letters:
+        freq = sum(mot.count(l) for mot in possible_words) / nombre_de_mots
+        if freq > best_freq:
+            best_freq = freq
+            best_letter = l
+    return f"Essayez de jouer la lettre '{best_letter}' !"
 
 
 
